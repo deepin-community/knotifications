bac9042e0b9fcf80879ab83fe7eabf8d9072698f
Deepin Developer <deepin-dev@deepin.org>
2022-09-22 17:41:41 +0800

    feat: Init commit
    init knotifications
    
    Log:

---
+++ src/notifybyandroid.h
@@ -0,0 +1,42 @@
+/*
+    SPDX-FileCopyrightText: 2018 Volker Krause <vkrause@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#ifndef NOTIFYBYANDROID_H
+#define NOTIFYBYANDROID_H
+
+#include "knotificationplugin.h"
+
+#include <QAndroidJniObject>
+#include <QPointer>
+
+/** Android notification backend. */
+class NotifyByAndroid : public KNotificationPlugin
+{
+    Q_OBJECT
+public:
+    explicit NotifyByAndroid(QObject *parent = nullptr);
+    ~NotifyByAndroid() override;
+
+    // interface of KNotificationPlugin
+    QString optionName() override;
+    void notify(KNotification *notification, KNotifyConfig *config) override;
+    void update(KNotification *notification, KNotifyConfig *config) override;
+    void close(KNotification *notification) override;
+
+    // interface from Java
+    void notificationFinished(int id);
+    void notificationActionInvoked(int id, int action);
+    void notificationInlineReply(int id, const QString &text);
+
+private:
+    void notifyDeferred(KNotification *notification);
+    QAndroidJniObject createAndroidNotification(KNotification *notification, KNotifyConfig *config) const;
+
+    QAndroidJniObject m_backend;
+    QHash<int, QPointer<KNotification>> m_notifications;
+};
+
+#endif // NOTIFYBYANDROID_H
